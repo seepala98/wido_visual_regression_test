f1ca5cda19e0ac3d7b580089ed6fc3d07990343a
seepala98 <seepala2@gmail.com>
2022-04-06 13:52:46 -0400

    fix baseurl from wido.config.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -96,7 +96,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'https://www.tangerine.ca/en/products',
+    baseUrl: 'https://www.tangerine.ca/en/',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
